9009651de89c3a98208468055203e42405e65272
Jaime Morais <jaime.morais@serpro.gov.br>
2022-12-23 08:36:18 -0300

    Implement home page

--- BlazorExpenseTracker/Program.cs
+++ BlazorExpenseTracker/Program.cs
@@ -1,21 +1,18 @@
-using BlazorExpenseTracker.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using BlazorExpenseTracker.Domain.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<ExpensesDataService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
